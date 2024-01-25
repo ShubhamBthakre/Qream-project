8ba8432119705fb6604125148640f5811f968521
ShubhamBthakre <132575491+ShubhamBthakre@users.noreply.github.com>
2024-01-25 13:38:43 +0530

    fixed bugs

--- src/component/Table/BasicTable.jsx
+++ src/component/Table/BasicTable.jsx
@@ -1,24 +1,16 @@
 import React, { useMemo } from "react";
-import { useTable, useSortBy, usePagination } from "react-table";
+import { useTable, useSortBy, usePagination, useRowSelect } from "react-table";
 import MOCK_DATA from "../../assets/MOCK_DATA.json";
 import { COLUMNS } from "./column";
 import "./BasicTable.css";
 import { AiOutlineSortAscending } from "react-icons/ai";
 import { TbSortDescendingLetters } from "react-icons/tb";
+import { Checkbox } from "./RowSelection";
 
 function BasicTable() {
   //to imporove performance we use useMemo
   const columns = useMemo(() => COLUMNS, []);
   const data = useMemo(() => MOCK_DATA, []);
-
-  const tableInstance = useTable(
-    {
-      columns,
-      data,
-    },
-    useSortBy,
-    usePagination
-  );
 
   const {
     getTableProps,
@@ -26,18 +18,42 @@ function BasicTable() {
     headerGroups,
     footerGroups,
     prepareRow,
+    setPageSize,
     page,
     nextPage,
     canNextPage,
     canPreviousPage,
     pageOptions,
     state,
+    gotoPage,
+    pageCount,
     previousPage,
-  } = tableInstance;
+    selectedFlatRows,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy,
+    usePagination,
+    useRowSelect,
+    (hooks) => {
+      hooks.visibleColumns.push((columns) => [
+        {
+          id: "selection",
+          Header: ({ getToggleAllRowsSelectedProps }) => (
+            <Checkbox {...getToggleAllRowsSelectedProps()} />
+          ),
+          Cell: ({ row }) => (
+            <Checkbox {...row.getToggleRowSelectedProps()} />
+          ),
+        },
+        ...columns,
+      ]);
+    }
+  );
 
-  const { pageIndex } = state;
-
-  console.log(tableInstance);
+  const { pageIndex, pageSize } = state;
 
   return (
     <table {...getTableProps} className="w-full border-collapse">
@@ -70,7 +86,7 @@ function BasicTable() {
         {page.map((row) => {
           prepareRow(row);
           return (
-            <tr {...row.getRowProps()} className="even:bg-gray-100">
+            <tr {...row.getRowProps()} onClick={()=>{}} className="even:bg-gray-100 cursor-pointer">
               {row.cells.map((cell) => (
                 <td {...cell.getCellProps()} className="p-2">
                   {cell.render("Cell")}{" "}
@@ -97,13 +113,41 @@ function BasicTable() {
             {pageIndex + 1} of {pageOptions.length}
           </strong>
         </span>
-        <button onClick={() => previousPage()} disabled={!previousPage}>
+
+        {/* <select
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[5, 10].map((pageSize) => (
+            <option key={pageSize}> show {pageSize}</option>
+          ))}
+        </select> */}
+
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
           Previos Page {"  "}
         </button>
-        <button onClick={() => nextPage()} disabled={!canNextPage}>
-          Next Page
+        <button onClick={() => nextPage()} disabled={!canNextPage} className="ml-3">
+         Next Page
+        </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
         </button>
       </div>
+
+      {/* <pre>
+        <code>
+          {JSON.stringify(
+            {
+              selectedFlatRows: selectedFlatRows.map(row => row.original)
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre> */}
     </table>
   );
 }
