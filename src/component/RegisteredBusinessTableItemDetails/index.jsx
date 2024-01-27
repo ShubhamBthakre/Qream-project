a259af8f5d621140e737817cde1c762e0e68cfa1
ShubhamBthakre <132575491+ShubhamBthakre@users.noreply.github.com>
2024-01-27 14:51:26 +0530

    added navigation to RegisteredBusinessTableItemDetails

--- src/component/RegisteredBusinessTableItemDetails/index.jsx
+++ src/component/RegisteredBusinessTableItemDetails/index.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import ItemCard from "../../RegisteredBusinessTableItemCard/index";
 import { IoMdArrowRoundBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 function RegisteredBusinessTableItemDetails() {
-
+  const navigate = useNavigate();
 
   const handleRedirect = () => {
-    history.goBack();
+    navigate("/registered-business")
   };
   return (
     <div className="w-full ">
       <div>
-        <IoMdArrowRoundBack onClick={()=>handleRedirect()}/>
+        <IoMdArrowRoundBack onClick={()=>handleRedirect()} className="cursor-pointer text-2xl"/>
       </div>
       <ItemCard />
     </div>
