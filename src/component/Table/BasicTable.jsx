dcfe47e633707fdbf02d13b8f1af4bd37f9ba45b
ShubhamBthakre <132575491+ShubhamBthakre@users.noreply.github.com>
2024-01-27 14:44:02 +0530

    Aligned table properly

--- src/component/Table/BasicTable.jsx
+++ src/component/Table/BasicTable.jsx
@@ -2,7 +2,6 @@ import React, { useMemo } from "react";
 import { useTable, useSortBy, usePagination, useRowSelect } from "react-table";
 import MOCK_DATA from "../../assets/MOCK_DATA.json";
 import { COLUMNS } from "./column";
-import "./BasicTable.css";
 import { AiOutlineSortAscending } from "react-icons/ai";
 import { TbSortDescendingLetters } from "react-icons/tb";
 import { Checkbox } from "./RowSelection";
@@ -37,7 +36,7 @@ function BasicTable() {
     }
   );
 
-  console.log(tableInstance)
+  console.log(tableInstance);
 
   const {
     getTableProps,
@@ -61,67 +60,68 @@ function BasicTable() {
   const { pageIndex, pageSize } = state;
 
   return (
-    <table {...getTableProps} className="w-full border-collapse table-fixed">
-      <thead className="text-center">
-        {headerGroups.map((headerGroup) => (
-          <tr
-            {...headerGroup.getHeaderGroupProps()}
-            className=""
-            key={headerGroup.id}
-          >
-            {headerGroup.headers.map((column) => (
-              <th
-                {...column.getHeaderProps(column.getSortByToggleProps())}
-                className="p-1 text-left text-sm md:text-base"
-                key={column.id}
-              >
-                {column.render("Header")}
-                <span>
-                  {column.isSorted ? (
-                    column.isSortedDesc ? (
-                      <TbSortDescendingLetters />
-                    ) : (
-                      <AiOutlineSortAscending />
-                    )
-                  ) : (
-                    ""
-                  )}
-                </span>
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps}>
-        {page.map((row) => {
-          prepareRow(row);
-          return (
+    <div>
+      <table {...getTableProps} className="w-full border-collapse table-fixed">
+        <thead className="text-center">
+          {headerGroups.map((headerGroup) => (
             <tr
-              {...row.getRowProps()}
-              onClick={() => setActiveNavbarTitle(Number(row.id) + 1)}
-              className="even:bg-gray-100 cursor-pointer hover:bg-sky-100"
-              key={row.id}
+              {...headerGroup.getHeaderGroupProps()}
+              className=""
+              key={headerGroup.id}
             >
-              {row.cells.map((cell) => (
-                <td
-                  {...cell.getCellProps()}
-                  className="p-1 w-auto text-sm md:text-base text-wrap"
-                  key={cell.id}
-                  onClick={() => setItemDetails(Number(row.id) + 1)}
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  className="p-1 text-left text-sm md:text-base"
+                  key={column.id}
                 >
-                  <NavLink
-                    to="/registered-business-Item-details"
-                    className={"w-full"}
-                  >
-                    {cell.render("Cell")}{" "}
-                  </NavLink>
-                </td>
+                  {column.render("Header")}
+                  <span>
+                    {column.isSorted ? (
+                      column.isSortedDesc ? (
+                        <TbSortDescendingLetters />
+                      ) : (
+                        <AiOutlineSortAscending />
+                      )
+                    ) : (
+                      ""
+                    )}
+                  </span>
+                </th>
               ))}
             </tr>
-          );
-        })}
-      </tbody>
-      {/* <tfoot>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr
+                {...row.getRowProps()}
+                onClick={() => setActiveNavbarTitle(Number(row.id) + 1)}
+                className="even:bg-gray-100 cursor-pointer hover:bg-sky-100"
+                key={row.id}
+              >
+                {row.cells.map((cell) => (
+                  <td
+                    {...cell.getCellProps()}
+                    className="p-1 w-auto text-sm md:text-base text-wrap"
+                    key={cell.id}
+                    onClick={() => setItemDetails(Number(row.id) + 1)}
+                  >
+                    <NavLink
+                      to="/registered-business-Item-details"
+                      className={"w-full"}
+                    >
+                      {cell.render("Cell")}{" "}
+                    </NavLink>
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+        {/* <tfoot>
         {footerGroups.map((footerGroup) => (
           <tr {...footerGroup.getFooterGroupProps()}>
             {footerGroup.headers.map((column) => (
@@ -131,51 +131,7 @@ function BasicTable() {
         ))}
       </tfoot> */}
 
-      <div className="flex w-full">
-        <span className="">
-          page {pageIndex + 1} of {pageOptions.length}
-        </span>
-
-        {/* <select
-          value={pageSize}
-          onChange={(e) => setPageSize(Number(e.target.value))}
-        >
-          {[5, 10].map((pageSize) => (
-            <option key={pageSize}> show {pageSize}</option>
-          ))}
-        </select> */}
-
-        <button
-          onClick={() => gotoPage(0)}
-          disabled={!canPreviousPage}
-          className="block"
-        >
-          {"<<"}
-        </button>
-        <button
-          onClick={() => previousPage()}
-          disabled={!canPreviousPage}
-          className="block"
-        >
-          Previos Page {"  "}
-        </button>
-        <button
-          onClick={() => nextPage()}
-          disabled={!canNextPage}
-          className="block ml-3"
-        >
-          Next Page
-        </button>
-        <button
-          onClick={() => gotoPage(pageCount - 1)}
-          disabled={!canNextPage}
-          className="block"
-        >
-          {">>"}
-        </button>
-      </div>
-
-      {/* <pre>
+        {/* <pre>
         <code>
           {JSON.stringify(
             {
@@ -186,7 +142,67 @@ function BasicTable() {
           )}
         </code>
       </pre> */}
-    </table>
+      </table>
+      <div className="flex align-centre gap-x-5 min-w-full text-lg mt-2">
+        <div>
+          Page <span className="font-semibold">{pageIndex + 1}</span> of{" "}
+          <span className="font-semibold">{pageOptions.length}</span>
+        </div>
+
+        {/* <select
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[5, 10].map((pageSize) => (
+            <option key={pageSize}> show {pageSize}</option>
+          ))}
+        </select> */}
+
+        <div className="flex gap-x-4 text-xl">
+          <div>
+            <button
+              onClick={() => gotoPage(0)}
+              disabled={!canPreviousPage}
+              className="cursor-pointer"
+            >
+              {"<< "}
+            </button>
+          </div>
+
+          <div className="flex gap-x-2">
+            <div>
+              <button
+                onClick={() => previousPage()}
+                disabled={!canPreviousPage}
+                className="cursor-pointer"
+              >
+                {" <"}
+              </button>
+            </div>
+
+            <div>
+              <button
+                onClick={() => nextPage()}
+                disabled={!canNextPage}
+                className="cursor-pointer"
+              >
+                {"> "}
+              </button>
+            </div>
+          </div>
+
+          <div>
+            <button
+              onClick={() => gotoPage(pageCount - 1)}
+              disabled={!canNextPage}
+              className="cursor-pointer"
+            >
+              {">>"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
