74d11c9d769dd9564da49aaa1a50c6835014191e
ShubhamBthakre <132575491+ShubhamBthakre@users.noreply.github.com>
2024-01-27 15:09:12 +0530

    some change

--- src/component/RegisteredBusinessTableItemDetails/index.jsx
+++ src/component/RegisteredBusinessTableItemDetails/index.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ItemCard from "../../RegisteredBusinessTableItemCard/index";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
+import { useBusinessContext } from "../../context/businessContext";
 
 function RegisteredBusinessTableItemDetails() {
+  const { setActiveNavbarTitle } = useBusinessContext();
   const navigate = useNavigate();
 
   const handleRedirect = () => {
     navigate("/registered-business")
+    setActiveNavbarTitle("Business Registered")
   };
   return (
     <div className="w-full ">
