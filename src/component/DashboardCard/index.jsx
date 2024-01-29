8c513eee35591b0c0370cc85821e5802a60831fc
ShubhamBthakre <132575491+ShubhamBthakre@users.noreply.github.com>
2024-01-29 15:08:42 +0530

    alignment done

--- src/component/DashboardCard/index.jsx
+++ src/component/DashboardCard/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function DashboardCard({ label, title, count }) {
   return (
-    <div className='w-full min-w-[100px] max-w-[350px] min-h-52 mb-4  mr-4 bg-light-sky rounded-xl flex flex-col p-5'>
+    <div className='w-full min-w-[100px] max-w-[300px] lg:max-w-[380px] min-h-52 mb-4  mr-4 bg-light-sky rounded-xl flex flex-col p-5'>
       <div className="w-full flex justify-end mb-2">
         <label>{label}</label>
         <select  className=" bg-light-sky">
